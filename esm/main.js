d682d4d0140d8709a0de534b503ae844ce725e15
杜帅(dushuai) <dushuai@qding.me>
2021-01-22 14:44:35 +0800

    change module to umd

--- esm/main.js
+++ esm/main.js
@@ -1,4 +1,16 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-createApp(App).mount("#app");
+(function (factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        var v = factory(require, exports);
+        if (v !== undefined) module.exports = v;
+    }
+    else if (typeof define === "function" && define.amd) {
+        define(["require", "exports", "vue", "./App.vue"], factory);
+    }
+})(function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    const vue_1 = require("vue");
+    const App_vue_1 = require("./App.vue");
+    vue_1.createApp(App_vue_1.default).mount("#app");
+});
 //# sourceMappingURL=main.js.map
